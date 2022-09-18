65bcd7df395815c7c11abeef81b7bc78e24af331
Felipe Alves <felipealvess_hpg_@hotmail.com>
2022-09-17 21:44:31 -0300

    Add project files.

---
+++ AtividadeProjetoDeSoftware/Material.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtividadeProjetoDeSoftware
+{
+    class Material
+    {
+        private string descricao;
+        private double preco;
+
+    }
+}
---
+++ AtividadeProjetoDeSoftware/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace AtividadeProjetoDeSoftware
+{
+    public static class Program
+    {
+        public static void Main (String[] args)
+        {
+
+        }
+    }
+}
